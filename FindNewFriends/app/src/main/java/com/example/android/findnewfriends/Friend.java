19c4d8e18b07473921052c17d3154ebadf8b68af
Michael Highsmith <mhighsm1@slu.edu>
2016-12-27 15:40:32 -0600

    Find New Friends with dummy inputs

--- FindNewFriends/app/src/main/java/com/example/android/findnewfriends/Friend.java
+++ FindNewFriends/app/src/main/java/com/example/android/findnewfriends/Friend.java
@@ -4,25 +4,25 @@ package com.example.android.findnewfriends;
  * Created by Owner on 12/27/2016.
  */
 
-public class Car {
-    private String make;
-    private int year;
+public class Friend {
+    private String user;
+    private int goats_sent;
     private int iconID;
     private String condition;
 
-    public Car(String make, int year, int iconID, String condition) {
+    public Friend(String user, int goats_sent, int iconID, String condition) {
         super();
-        this.make = make;
-        this.year = year;
+        this.user = user;
+        this.goats_sent = goats_sent;
         this.iconID = iconID;
         this.condition = condition;
     }
 
-    public String getMake() {
-        return make;
+    public String getUser() {
+        return user;
     }
-    public int getYear() {
-        return year;
+    public int getGoats_sent() {
+        return goats_sent;
     }
     public int getIconID(){
         return iconID;
