7233b1431857b72b9c2bbf980023cb8fad0faa30
Michael Highsmith <mhighsm1@slu.edu>
2016-12-29 16:25:26 -0600

    swipe screen practice

---
+++ SwipeScreenPractice/app/src/main/java/com/example/android/swipescreenpractice/PageFragment.java
@@ -0,0 +1,34 @@
+package com.example.android.swipescreenpractice;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link PageFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link PageFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class PageFragment extends android.support.v4.app.Fragment {
+    TextView textView;
+
+    public PageFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.page_fragment_layout, container, false);
+        textView = (TextView)view.findViewById(R.id.textView);
+        Bundle bundle = getArguments();
+        String message = Integer.toString(bundle.getInt("count"));
+        textView.setText("This is the "+message+ "Swipe View Page");
+        return view;
+    }
+}
